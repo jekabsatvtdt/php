1da78faf5a1a22fad02d336636a47a5c0a08f3c4
jekabsatvtdt <116027757+jekabsatvtdt@users.noreply.github.com>
2022-10-17 22:15:34 +0300

    TODO renamed

--- todo_list.php
+++ todo_list.php
@@ -6,7 +6,7 @@ $table = "todo_list";
 
 try {
 	$db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
-	echo "<h2>TODO</h2><ol>";
+	echo "<h2>Darāmie darbi</h2><ol>";
 	foreach($db->query("SELECT content FROM $table") as $row) {
 		echo "<li>" . $row["content"] . "</li>";
 	}
